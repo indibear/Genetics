1bcc3ec3000cb9c210a50deff3283fa74e146859
Peet Denny <peet.denny@wealthwizards.com>
2018-07-15 18:35:16 +0100

    added Genotype factory method

--- js/genetics.js
+++ js/genetics.js
@@ -1,6 +1,22 @@
-class Genome {
-    constructor(base, dilution) {
-      this.base = base;
-      this.dilution = dilution;
-    }
+class QuirshiGenome {
+  static randomGenotype(){
+    var baseAlleles = ['black', 'brown', 'cinnamon'];
+    var dilutionAlleles = ['on', 'off'];
+    var base = baseAlleles[Math.floor(Math.random() * baseAlleles.length)]
+    var dilution = dilutionAlleles[Math.floor(Math.random() * dilutionAlleles.length)]
+    return new QuirshiGenome(base, dilution);
+  }
+  constructor(base, dilution) {
+    this.base = base;
+    this.dilution = dilution;
+  }
 }
+
+class Quirshi {
+  constructor(genome){
+    this.genome = genome;
+  }
+}
+
+genome = QuirshiGenome.randomGenotype();
+console.log(genome);
