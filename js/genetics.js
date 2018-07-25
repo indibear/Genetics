3d8a8c3c221826da4bfd4ba41122cf51e136ce82
indibear <isdenny123@gmail.com>
2018-07-25 20:42:55 +0100

    Working on programaticaly setting colour, milestone 1 hit

--- js/genetics.js
+++ js/genetics.js
@@ -27,76 +27,83 @@ class Quirshi {
     this.genome = genome;
   }
 }
-/*quirshiPopulation = [];
-for (var i = 0; i <1; i ++){
+
+quirshiPopulation = [];
+var imgs=[];
+
+for (var i = 0; i <10; i ++){
   genome = QuirshiGenome.randomGenotype();
   quirshiPopulation.push(genome);
 }
-console.log(quirshiPopulation);
-for (q of quirshiPopulation) {
-  pop = document.getElementById("population");
-  var span = document.createElement("SPAN");
-  span.classList.add("quirshiPanel");
-  if(q.dilution[0] == 0){
-    span.style.backgroundColor = baseColours[q.base[0]];
-  }else{
-    span.style.backgroundColor = diluteColours[q.base[0]];
-  }
-  var textnode = document.createTextNode(baseAlleles[q.base[0]] + " | " + (baseAlleles[q.base[1]]));
-  span.appendChild(textnode);
-  pop.appendChild(span);
-*/
-  var canvas=document.getElementById("quirshi1");
-  var ctx=canvas.getContext("2d");
-
-  var imageURLs=[];
-   var imagesOK=0;
-   var imgs=[];
-   imageURLs.push("imgs/QuirshiBase.png");
-   imageURLs.push("imgs/QuirshiColour.png");
-   imageURLs.push("imgs/QuirshiLines.png");
 
 
-   function loadAllImages(){
-       for (var i = 0; i < imageURLs.length; i++) {
-         var img = new Image();
-         imgs.push(img);
-         img.onload = function(){ imagesOK++; imagesAllLoaded(); };
-         img.src = imageURLs[i];
-       }
+var imageURLs=[];
+var imagesOK=0;
+imageURLs.push("imgs/QuirshiBase.png");
+imageURLs.push("imgs/QuirshiColour.png");
+imageURLs.push("imgs/QuirshiLines.png");
+
+
+function loadAllImages(){
+   for (var i = 0; i < imageURLs.length; i++) {
+     var img = new Image();
+     imgs.push(img);
+     img.onload = function(){ imagesOK++; imagesAllLoaded(); };
+     img.src = imageURLs[i];
    }
+}
 
-   var imagesAllLoaded = function() {
-     if (imagesOK==imageURLs.length ) {
-        // all images are fully loaded an ready to use
-        base=imgs[0];
-        colour=imgs[1];
-        lines=imgs[2];
-        console.log("imagesAllLoaded");
-        start();
-     }
-   };
-
-   function start(){
-
-        // save the context state
-        ctx.save();
-
-        // draw the lines
-        ctx.drawImage(base,0, 0);
-
-        ctx.globalCompositeOperation="source-in";
-        ctx.fillStyle="#2f3030";
-        ctx.fillRect(0,0,canvas.width,canvas.height);
-
-         ctx.globalCompositeOperation="source-atop";
-         ctx.drawImage(colour,0,0);
-
-        ctx.globalCompositeOperation="multiply";
-        ctx.drawImage(lines,0,0);
+var imagesAllLoaded = function() {
+ if (imagesOK==imageURLs.length ) {
+    // all images are fully loaded an ready to use
+    base=imgs[0];
+    spots=imgs[1];
+    lines=imgs[2];
+    renderVillage();
+ }
+}
 
 
-        ctx.restore();
-          console.log("here2")
-    }
-   loadAllImages();
+function renderVillage(){
+  pop = document.getElementById("population");
+  for (q of quirshiPopulation) {
+    var span = document.createElement("SPAN");
+    span.classList.add("quirshiPanel");
+    // if(q.dilution[0] == 0){
+    //   span.style.backgroundColor = baseColours[q.base[0]];
+    // }else{
+    //   span.style.backgroundColor = diluteColours[q.base[0]];
+    // }
+    //var textnode = document.createTextNode(baseAlleles[q.base[0]] + " | " + (baseAlleles[q.base[1]]));
+    //span.appendChild(textnode);
+    pop.appendChild(span);
+    var canvas = document.createElement("CANVAS");
+    renderQuirshi(canvas, imgs, 0, 0, 200, 200);
+    span.appendChild(canvas);
+  }
+}
+
+function renderQuirshi(canvas, imgs, x, y, width, height){
+  base=imgs[0];
+  spots=imgs[1];
+  lines=imgs[2];
+  var ctx=canvas.getContext("2d");
+  ctx.save();
+
+  ctx.drawImage(base,0, 0, width, height);
+
+  ctx.globalCompositeOperation="source-in";
+  ctx.fillStyle="#2f3030";
+  ctx.fillRect(0,0,canvas.width,canvas.height);
+
+  ctx.globalCompositeOperation="source-atop";
+  ctx.drawImage(spots, 0, 0, width, height);
+
+  ctx.globalCompositeOperation="multiply";
+  ctx.globalCompositeOperation="multiply";
+  ctx.drawImage(lines, 0, 0, width, height);
+  ctx.restore();
+
+};
+
+loadAllImages();
