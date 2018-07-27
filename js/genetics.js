b7f44c40e187539394a6fe6b435a2565571a11dd
indibear <isdenny123@gmail.com>
2018-07-27 22:08:15 +0100

    WHite SPotting Added and genome display

--- js/genetics.js
+++ js/genetics.js
@@ -4,6 +4,8 @@ var diluteColours = ['#83898c', '#8e847c', '#d8c5b6' ]
 var baseAlleles = ['coal', 'bark', 'cork'];
 var dilutionAlleles = ['dense', 'dilute'];
 
+var spottingAlleles = ["high", "low", "off"];
+
 class QuirshiGenome {
   static selectRandomElementPair(arr){
     var elementPair = [Math.floor(Math.random() * arr.length),Math.floor(Math.random() * arr.length)];
@@ -13,12 +15,14 @@ class QuirshiGenome {
   static randomGenotype(){
     var base = QuirshiGenome.selectRandomElementPair(baseAlleles);
     var dilution = QuirshiGenome.selectRandomElementPair(dilutionAlleles);
-    return new QuirshiGenome(base, dilution);
+    var spotting = QuirshiGenome.selectRandomElementPair(spottingAlleles);
+    return new QuirshiGenome(base, dilution, spotting);
   }
 
-  constructor(base, dilution) {
+  constructor(base, dilution, spotting) {
     this.base = base;
     this.dilution = dilution;
+    this.spotting = spotting;
   }
 }
 
@@ -40,7 +44,8 @@ for (var i = 0; i <50; i ++){
 var imageURLs=[];
 var imagesOK=0;
 imageURLs.push("imgs/QuirshiBase.png");
-imageURLs.push("imgs/QuirshiColour.png");
+imageURLs.push("imgs/QuirshiSpottingHigh.png");
+imageURLs.push("imgs/QuirshiSpottingLow.png");
 imageURLs.push("imgs/QuirshiLines.png");
 
 
@@ -57,8 +62,9 @@ var imagesAllLoaded = function() {
  if (imagesOK==imageURLs.length ) {
     // all images are fully loaded an ready to use
     base=imgs[0];
-    spots=imgs[1];
-    lines=imgs[2];
+    highSpots=imgs[1];
+    lowSpots=imgs[2];
+    lines=imgs[3];
     renderVillage();
  }
 }
@@ -66,42 +72,63 @@ var imagesAllLoaded = function() {
 
 function renderVillage(){
   pop = document.getElementById("population");
-  for (q of quirshiPopulation) {
+  for (quirshi of quirshiPopulation) {
     var span = document.createElement("SPAN");
     span.classList.add("quirshiPanel");
-    var backgroundColor = "black";
-    if(q.dilution[0] == 0){
-      backgroundColor = baseColours[q.base[0]];
-    }else{
-      backgroundColor = diluteColours[q.base[0]];
-    }
     //var textnode = document.createTextNode(baseAlleles[q.base[0]] + " | " + (baseAlleles[q.base[1]]));
     //span.appendChild(textnode);
     pop.appendChild(span);
     var canvas = document.createElement("CANVAS");
     canvas.classList.add("quirshiCanvas");
-    renderQuirshi(canvas, imgs, 0, 0, 200, 110, backgroundColor);   //image size is 700x and 387y
+    renderQuirshi(canvas, imgs, 0, 0, quirshi);   //image size is 700x and 387y
     span.appendChild(canvas);
+    var textSpan = document.createElement("SPAN");
+    span.classList.add("quirshiTextPanel");
+    console.log(quirshi.base)
+    textSpan.innerText= "[" + baseAlleles[quirshi.base[0]]+
+                        " "+ baseAlleles[quirshi.base[1]]+ "]\n"+
+                        spottingAlleles[quirshi.spotting[0]] + " " +
+                        spottingAlleles[quirshi.spotting[1]] + "\n" +
+                        "[" + dilutionAlleles[quirshi.dilution[0]]+
+                                            " "+ dilutionAlleles[quirshi.dilution[1]]+ "]";
+    span.appendChild(textSpan);
+
   }
 }
 
-function renderQuirshi(canvas, imgs, x, y, width, height, colour){
+function renderQuirshi(canvas, imgs, x, y, quirshi){
   base=imgs[0];
-  spots=imgs[1];
-  lines=imgs[2];
+  highSpots=imgs[1];
+  lowSpots=imgs[2];
+  lines=imgs[3];
+  width=200;
+  height=110;
+
   var ctx=canvas.getContext("2d");
   ctx.save();
 
   ctx.drawImage(base,0, 0, width, height);
 
+  var backgroundColour = "black";
+  if(quirshi.dilution[0] == 0){
+    backgroundColour = baseColours[quirshi.base[0]];
+  }else{
+    backgroundColour = diluteColours[quirshi.base[0]];
+  }
+
   ctx.globalCompositeOperation="source-in";
-  ctx.fillStyle=colour;
+  ctx.fillStyle=backgroundColour;
   ctx.fillRect(0,0,canvas.width,canvas.height);
 
   ctx.globalCompositeOperation="source-atop";
-  ctx.drawImage(spots, 0, 0, width, height);
+  switch(quirshi.spotting[0]){
+    case 0:
+      ctx.drawImage(highSpots, 0, 0, width, height);
+    case 1:
+      ctx.drawImage(lowSpots, 0, 0, width, height);
+  }
 
-  ctx.globalCompositeOperation="multiply";
+
   ctx.globalCompositeOperation="multiply";
   ctx.drawImage(lines, 0, 0, width, height);
   ctx.restore();
